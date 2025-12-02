d443598bddf21ee479618d533ab19d5dadda376f
eekatzee18 <kuptsova.98@list.ru>
2025-12-02 10:43:09 +0400

    resolved

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,24 @@
+const editor = document.getElementById('editor');
+const card = document.querySelector('.card');
+const btn = document.createElement('button');
+btn.textContent = 'Очистить содержимое';
+card.appendChild(btn);
+
+btn.addEventListener('click', function(){
+    editor.value = '';
+    localStorage.removeItem('editor');
+})
+editor.addEventListener('input', saveText)
+
+window.addEventListener('load', loadText)
+
+function loadText(){
+    const savedText = localStorage.getItem('editor');
+    if(savedText){
+        editor.value = savedText;
+    }
+}
+
+function saveText(){
+    localStorage.setItem('editor',editor.value);
+}
